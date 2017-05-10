624a0e7b726d76652f6f7f3d98fc9e1adce503b4
Carl Hain <crhain@gmail.com>
2017-05-10 17:21:57 -0500

    Add ability to make search requests to bing api

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const fetch = require('node-fetch');
 const express = require('express');
 const port = process.env.PORT || 5000;
 const app = express();
 var searchHistory = [];
+var apiKey = process.env.SEARCH_API_KEY;
 
 app.use(express.static('public'));
 
@@ -13,9 +15,20 @@ app.get('/api/imagesearch/:search', (request, response) =>{
     offset = +offset || 10;
 
     //execute api call using a promise and fetch and then show results once they are returned
+    //should return imageURL, pageURL, alt text
+    //BING image api:
+     //results.totalEstimatedMatches
+     //results.value array:
+        //contentUrl = imageURL
+        //homePageUrl = pageURL
+        //name = alt text
     updateSearchHistory(searchJSON);
+    makeSearchRequest(search, offset)
+        .then( res => res.json())
+        .then(json => {
+            console.log(json);
+        });
     
-
     response.end('Executing image search: ' + search + ' using page offset of ' + offset );
 });
 
@@ -32,5 +45,18 @@ function updateSearchHistory(search, limit = 10){
         searchHistory = [];
     }
     searchHistory.push(search);
+}
+
+function makeSearchRequest(search, offset){
+    let url = "https://api.cognitive.microsoft.com/bing/v5.0/images/search";
+    let myHeaders = {
+        "Ocp-Apim-Subscription-Key": apiKey        
+    };
+    let myInit = {
+        method: 'GET',
+        headers: myHeaders
+    };
+
+   return fetch(url, myInit);
 }
 
