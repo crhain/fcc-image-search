c7391846ed8b9a53a81294935abf23ad28075429
Carl Hain <crhain@gmail.com>
2017-05-10 16:01:44 -0500

    Returns search history of limited size when calling api/latest/imagesearch

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 const express = require('express');
 const port = process.env.PORT || 5000;
 const app = express();
+var searchHistory = [];
 
 app.use(express.static('public'));
 
 app.get('/api/imagesearch/:search', (request, response) =>{    
     var search = request.params.search,
-        offset = request.query.offset;
+        offset = request.query.offset,
+        searchJSON = {term: search, when: new Date().toISOString()};
     //set default offset value if none passed
     offset = +offset || 10;
 
     //execute api call using a promise and fetch and then show results once they are returned
+    updateSearchHistory(searchJSON);
     
 
     response.end('Executing image search: ' + search + ' using page offset of ' + offset );
 });
 
-app.get('/api/latest/imagesearch/', (request, response) =>{
-    response.end('Getting latest image searches!');
+app.get('/api/latest/imagesearch/', (request, response) =>{    
+    response.end(JSON.stringify(searchHistory));
 });
 
 app.listen(port, ()=>{
     console.log('Running server on port: ' + port);
 });
+
+function updateSearchHistory(search, limit = 10){
+    if(searchHistory.length >= limit){
+        searchHistory = [];
+    }
+    searchHistory.push(search);
+}
 
